9524c97d26fa78c9fdbb352690dffbba743f429f
alan <alan@localhost.(none)>
2009-11-11 15:49:08 +0000

    linux build compat

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 CC=gcc
 CFLAGS=-g -Wall
+LIB=-lm
 
 OBJECTS=rpn.o stack.o lex.o 
 
 compile: $(OBJECTS)
-	$(CC) $(CFLAGS) -o rpn $(OBJECTS)
+	$(CC) $(CFLAGS) $(LIB) -o rpn $(OBJECTS)
 	sh tests.sh
 
 all: compile
--- tests.sh
+++ tests.sh
@@ -3,9 +3,9 @@
 function try {
   local res=$(echo "$1 ." | ./rpn)
   if [ $res = $2 ]; then
-    echo "\e[32;40m[ ✔ ]\033[0m\t$res = $1"
+    echo -e "\e[32;40m[ ✔ ]\033[0m\t$res = $1"
   else
-    echo "\e[31;40m[ ✖ ]\033[0m\t$res != $1, should be $2" >&2
+    echo -e "\e[31;40m[ ✖ ]\033[0m\t$res != $1, should be $2" >&2
     exit 1
   fi
 }
